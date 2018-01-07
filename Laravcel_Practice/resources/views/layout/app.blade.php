6cfbded44d24835300170ca14f548a043de33422
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-01-07 17:49:57 +0600

    page link added

--- Laravcel_Practice/resources/views/layout/app.blade.php
+++ Laravcel_Practice/resources/views/layout/app.blade.php
@@ -86,11 +86,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/')}}">Home</a>
+                    <a href="{{ url('about')}}">About</a>
+                    <a href="{{ url('contact')}}">Contact</a>
+                    <a href="{{ url('join')}}">Join</a>
+                    <a href="{{ url('songs')}}">Songs</a>
                 </div>
             </div>
         </div>
